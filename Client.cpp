e2531fffa50a42252b210ecda4910b0aedd476fc
incredibleLeitman <michael.leithner@gmail.com>
2020-11-03 15:06:49 +0100

    test socket connection on unix and windows

--- Client.cpp
+++ Client.cpp
@@ -1,44 +1,92 @@
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
+#define DEBUG_OUTPUT
+
+#ifdef _WIN32
+    // link with Ws2_32.lib
+    #pragma comment(lib,"Ws2_32.lib")
+
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+#else
+    #include <sys/socket.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+#endif
+#include <stdio.h>
 #include <string.h>
 #include <string>
+#ifdef DEBUG_OUTPUT
+    #include <iostream>
+#endif
 
 #include "Client.h"
 
-Client::Client(std::string ip, int port)
-{ 
-    struct sockaddr_in serv_addr; 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-      throw "Socket creation error.";
-    } 
-   
-    serv_addr.sin_family = AF_INET; 
+
+Client::Client(std::string ip, int port) {
+
+#ifdef DEBUG_OUTPUT
+    std::cout << "init listening socket on " << ip << " port " << port << std::endl;
+#endif
+
+#ifdef _WIN32
+    //WSADATA wsaData;   // if this doesn't work
+    WSAData wsaData; // then try this instead
+
+    // MAKEWORD(1,1) for Winsock 1.1, MAKEWORD(2,0) for Winsock 2.0
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
+        fprintf(stderr, "WSAStartup failed.\n");
+        exit(1);
+    }
+#endif
+
+    struct sockaddr_in serv_addr;
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+#ifdef DEBUG_OUTPUT
+        std::cout << "Socket creation error: " << sock << std::endl;
+#endif
+        throw "Socket creation error.";
+    }
+
+    serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = port;
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if (inet_pton(AF_INET, ip.c_str(), &serv_addr.sin_addr) <= 0)  
-    { 
+    //serv_addr.sin_addr.s_addr = inet_addr(ip.c_str()); // deprecated -> use inet_pton instead
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    int status = 0;
+    if ((status = inet_pton(AF_INET, ip.c_str(), &serv_addr.sin_addr)) <= 0)
+    {
+#ifdef DEBUG_OUTPUT
+        std::cout << "Invalid address/ Address not supported > status: " << status << std::endl;
+#endif
         throw "Invalid address/ Address not supported";
-    } 
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        throw "Connection Failed"; 
+    }
+
+    if ((status = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) < 0)
+    {
+#ifdef DEBUG_OUTPUT
+        std::cout << "Connection failed! > status: " << status << " errno: " << errno << std::endl;
+#endif
+        throw "Connection Failed";
     }
 }
 
 Client::~Client() {
+#ifdef _WIN32
+    closesocket(sock);
+#else
     close(sock);
+#endif
 }
 
 std::string Client::readString() {
+#ifdef _WIN32
+    recv(sock, buffer, READ_DATA_SIZE, 0);
+#else
     read(sock, buffer, READ_DATA_SIZE);
+#endif
     return std::string(buffer);
 }
 
 void Client::writeString(std::string input) {
-    send(sock, input.c_str(), input.length(), 0); 
+    send(sock, input.c_str(), input.length(), 0);
 }
--- Client.h
+++ Client.h
@@ -4,13 +4,13 @@ const int READ_DATA_SIZE = 2048;
 
 class Client {
 public:
-  Client(std::string ip, int port);
-  ~Client();
+    Client(std::string ip, int port);
+    ~Client();
 
-  std::string readString();
-  void writeString(std::string input);
+    std::string readString();
+    void writeString(std::string input);
 
 private:
-  int sock;
-  char buffer[READ_DATA_SIZE] = {0}; 
+    int sock;
+    char buffer[READ_DATA_SIZE] = {0};
 };
