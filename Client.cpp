40e7f99e62faa61eb787251e9648dcd51a6d1597
incredibleLeitman <michael.leithner@gmail.com>
2020-11-25 08:23:46 +0100

    parsing board everytime before checking message types; var mode not needed -> is only used for defined moves; var in_game not needed -> receive string is cleared after sending;

--- Client.cpp
+++ Client.cpp
@@ -22,7 +22,7 @@
 #include "Board.h"
 #include "Client.h"
 
-#define BOARD_LENGTH 380 // SERVER_V2 total gridsize including whitespaces
+#define BOARD_LENGTH     380 // SERVER_V2 total gridsize including whitespaces
 #define MSG_BOARD_HEADER "   0   1   2   3   4   5   6   7   8" // defines start of board
 //"************************ Player 2 won!**********************"
 #define GAME_OVER " won!**********************" // end of game
@@ -39,7 +39,7 @@ std::map<std::string, std::string> message_write_map {
   {"User\r\n\r\nPlease choose your mode [0-2]", "1"},
   {"Please choose wether you want the AI to start or not [0-1]", "1"},
   {"Do you want to continue with your turn [Y/N]?", "Y"},
-  {"Do you want to surrender [Y/N]?", "Y"}};
+  {"Do you want to surrender [Y/N]?", "N"}};
 
 Client::Client(std::string ip, int port)
 {
@@ -138,11 +138,18 @@ void Client::Start()
   size_t pos = std::string::npos;
   while (true)
   {
+    // read incoming commands from server
     m_strRecv.append(ReadString());
 
-    EMove mode;
+    // always parse in-game state if present and pass the game data to the Board
+    if ((pos = m_strRecv.rfind(MSG_BOARD_HEADER)) != std::string::npos)
+    {
+      std::string field = m_strRecv.substr(pos, BOARD_LENGTH);
 
-    bool in_game = false;
+      // remove newlines for combined parsing depending on different server versions
+      field.erase(remove(field.begin(), field.end(), ' '), field.end());
+      m_board->Parse(field);
+    }
 
     // Check which gameplay selection this string corresponds to (if any)
     // and set the `mode` accordingly
@@ -150,28 +157,13 @@ void Client::Start()
     {
       if (m_strRecv.rfind(message_and_move.first) != std::string::npos)
       {
-        mode    = message_and_move.second;
-        in_game = true;
-
-        // Since we found an in-game state, pass the game data to the Board
-        if ((pos = m_strRecv.rfind(MSG_BOARD_HEADER)) != std::string::npos)
-        {
-          std::string field = m_strRecv.substr(pos, BOARD_LENGTH);
-
-          // remove newlines for combined parsing depending on different server versions
-          field.erase(remove(field.begin(), field.end(), ' '), field.end());
-          m_board->Parse(field);
-        }
-
-        // usleep(300000);
-
-        std::string input = m_board->GetPosition(mode);
+        std::string input = m_board->GetPosition(message_and_move.second);
         WriteString(input);
       }
     }
 
     // If that wasn't an in-game string, we're in the menu
-    if (!in_game)
+    if (m_strRecv.length() > 0)
     {
       // Check which menu point this string corresponds to and send the result
       for (const auto& message_and_write : message_write_map)
@@ -182,9 +174,10 @@ void Client::Start()
         }
       }
 
+      // end of game detected, one player won
       if (m_strRecv.rfind(GAME_OVER) != std::string::npos)
       {
-          return;
+        return;
       }
     }
   }
