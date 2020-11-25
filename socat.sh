5c16ef38889ecd5e11b8ab6a6449a7ca6e451418
Karl <karl.bittner@hexaquo.at>
2020-11-25 14:53:35 +0100

    Socat: run server in background

--- socat.sh
+++ socat.sh
@@ -1,2 +1,2 @@
-socat TCP4-LISTEN:4455,reuseaddr EXEC:"./Fanorona",end-close,pty
+socat TCP4-LISTEN:4455,reuseaddr EXEC:"./Fanorona",end-close,pty &
 socat -v TCP4-CONNECT:localhost:4455 EXEC:"./funorona"
