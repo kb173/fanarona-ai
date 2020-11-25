3cbe8510e69774b94f1c88179a8d3a724121371c
karl <karl.bittner@hexaquo.at>
2020-11-25 21:24:11 +0100

    Save output and break if lost in test_wins.sh
    this allows us to debug in case of a loss

--- tools/test_wins.sh
+++ tools/test_wins.sh
@@ -5,7 +5,15 @@ loses=0
 
 while true
 do
-    ./socat.sh 2>&1 | grep -q "Player 2 won" && wins=$((wins+1)) || loses=$((loses+1))
+    ./socat.sh 2>&1 | tee lose_output.txt | grep -q "Player 2 won" && wins=$((wins+1)) || loses=$((loses+1))
+    
     echo "$wins wins vs $loses loses"
+    
+    if (( loses > 0 )); then
+        break
+    else
+        rm lose_output.txt
+    fi
+    
     killall Fanorona
 done
