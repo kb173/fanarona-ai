0af463eed5dff0d90c28f79193650a146bbcf36a
incredibleLeitman <michael.leithner@gmail.com>
2020-11-27 20:20:36 +0100

    added starting time to run bash scrips

--- tools/run-multiple.sh
+++ tools/run-multiple.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
 times=$1
+
+echo "$(date +"%T") running run_multiple - $1..."
+
 mkdir results
 for i in $(seq 1 1 $times); do
   echo "Run $i (saving result in results/$i.txt)"
-  ./funorona > results/$i.txt
+  ./funorona > results/$i.txt # run socketclient
+  #./socat.sh 2>&1 | tee results/$i.txt # run localclient
 done
--- tools/test_wins.sh
+++ tools/test_wins.sh
@@ -3,17 +3,19 @@
 wins=0
 loses=0
 
+echo "$(date +"%T") running test_wins..."
+
 while true
 do
     ./socat.sh 2>&1 | tee lose_output.txt | grep -q "Player 2 won" && wins=$((wins+1)) || loses=$((loses+1))
-    
+
     echo "$wins wins vs $loses loses"
-    
+
     if (( loses > 0 )); then
         break
     else
         rm lose_output.txt
     fi
-    
+
     killall Fanorona &>/dev/null
 done
