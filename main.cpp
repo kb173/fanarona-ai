9229ebeed0b6f1f1f09216ca0dbcc0479e285568
AmonShokhinAhmed <amon.shokhin.ahmed@gmail.com>
2020-12-11 17:44:05 +0100

    Apply coding convention to main

--- main.cpp
+++ main.cpp
@@ -9,65 +9,72 @@
 #include "LocalClient.h"
 #include "RemoteClient.h"
 
+// ////////////////////////////////////////////////////////////////////////
+// ShowStartUpImage
+//
+// Prints an ASCII image to the error output on start up
 void ShowStartUpImage()
 {
-  std::string line;
-  std::ifstream image("startupImage.txt");
-  if (image.is_open())
+  std::string strLine;
+  std::ifstream ssImage("startupImage.txt");
+  if (ssImage.is_open())
   {
-    while (getline(image, line))
+    while (getline(ssImage, strLine))
     {
-      std::cerr << line << std::endl;
+      std::cerr << strLine << std::endl;
     }
-    image.close();
+    ssImage.close();
   }
   else
   {
     std::cerr << "Unable to open startup image" << std::endl;
   }
 }
-
+// ////////////////////////////////////////////////////////////////////////
+// main
+//
+//
 int main(int argc, char** argv)
 {
   ShowStartUpImage();
-  EMode mode = EMode::AI;
-  int depth  = 3;
+  EMode eMode = EMode::AI;
+  int nDepth  = 3;
   // default values for server connection
   // "178.32.219.65", 4455 // Kajetan Test server - alpaga.hammerle.me
   // "10.64.99.107", 4455 // Lars' Fanorona Server
   // "127.0.0.1", 8888 // local Test server
-  std::string ip = "178.32.219.65";
-  int port       = 4455;
-  bool local     = true;
+  std::string strIp = "178.32.219.65";
+  int nPort         = 4455;
+  bool bLocal       = true;
   for (int i = 0; i < argc; ++i)
   {
     if (strcmp(argv[i], "--human") == 0)
     {
-      mode = EMode::HUMAN;
+      eMode = EMode::HUMAN;
     }
     else if (strcmp(argv[i], "--depth") == 0)
     {
-      depth = std::stoi(argv[i + 1]);
+      nDepth = std::stoi(argv[i + 1]);
     }
     else if (strcmp(argv[i], "--ip") == 0)
     {
-      ip = argv[i + 1];
+      strIp = argv[i + 1];
     }
     else if (strcmp(argv[i], "--remote") == 0)
     {
-      local = false;
+      bLocal = false;
     }
     else if (strcmp(argv[i], "--port") == 0)
     {
-      port = std::stoi(argv[i + 1]);
+      nPort = std::stoi(argv[i + 1]);
     }
   }
 
   // our game board -> gets filled by server messages, calculates next position...
-  auto board = std::make_shared<Board>(mode, depth);
+  auto board = std::make_shared<Board>(eMode, nDepth);
 
-  auto start = std::chrono::high_resolution_clock::now();
-  if (local)
+  auto clockStart = std::chrono::high_resolution_clock::now();
+  if (bLocal)
   {
     LocalClient client = LocalClient();
     client.SetBoard(board);
@@ -76,13 +83,13 @@ int main(int argc, char** argv)
   }
   else
   {
-    RemoteClient client(ip, port);
+    RemoteClient client(strIp, nPort);
     client.SetBoard(board);
     client.Start();
     std::cerr << "turns played: " << client.GetTurnsPlayed() << std::endl;
   }
-  std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now() - start;
-  std::cerr << "time spent: " << std::setprecision(3) << std::fixed << diff.count() << "s"
+  std::chrono::duration<double> clockDiff = std::chrono::high_resolution_clock::now() - clockStart;
+  std::cerr << "time spent: " << std::setprecision(3) << std::fixed << clockDiff.count() << "s"
             << std::endl;
 
   return 0;
